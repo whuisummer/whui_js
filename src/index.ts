54f124b6033c5bb13cd09d5ab0dc5ff8d41e5f06
王辉 <hwang090@mail.nfsq.com.cn>
2021-09-13 10:44:34 +0800

    目录调整

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,6 @@
  * @Autor: hwang
  * @Date: 2021-08-16 14:28:31
  * @LastEditors: hwang
- * @LastEditTime: 2021-09-02 09:13:29
+ * @LastEditTime: 2021-09-13 10:42:29
  */
-export { default as Foo } from './Foo';
-var cowsay = require('cowsay');
-
-console.log(
-  cowsay.say({
-    text: "I'm a moooodule",
-    e: 'oO',
-    T: 'U ',
-  }),
-);
+export { default as Foo } from './问题记录';
